8d4a4ec6027d7218df5985180f05df0decd4d0b4
kendol Pérez <47223224+kendol07@users.noreply.github.com>
2025-08-15 19:27:15 -0400

    Display cart items and total

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,5 @@
+import { products } from './products.js';
+
 const CART_KEY = 'rodo_cart';
 
 function getCart() {
@@ -16,4 +18,28 @@ export function addToCart(id) {
 
 export function getCartCount() {
   return getCart().length;
+}
+
+export function getCartItems() {
+  const counts = {};
+  getCart().forEach(id => {
+    counts[id] = (counts[id] || 0) + 1;
+  });
+  return Object.entries(counts).map(([id, qty]) => {
+    const product = products.find(p => p.id === parseInt(id, 10));
+    return { ...product, qty };
+  }).filter(item => item.id);
+}
+
+export function getCartTotal() {
+  return getCartItems().reduce((sum, item) => sum + item.price * item.qty, 0);
+}
+
+export function removeFromCart(id) {
+  const cart = getCart();
+  const index = cart.indexOf(id);
+  if (index !== -1) {
+    cart.splice(index, 1);
+    saveCart(cart);
+  }
 }
