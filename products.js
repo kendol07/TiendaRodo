c19fb3ba3babd170834c62ebb8829dc17f390d0d
kendol Pérez <47223224+kendol07@users.noreply.github.com>
2025-08-15 15:30:21 -0400

    Add modular catalog and cart system

---
+++ products.js
@@ -0,0 +1,44 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Ropa de verano',
+    price: 14.99,
+    category: 'Ropa',
+    image: 'https://sydney.pe/wp-content/uploads/2023/09/ropa-para-verano.png'
+  },
+  {
+    id: 2,
+    name: 'Lámpara LED',
+    price: 11.49,
+    category: 'Electrónica',
+    image: 'https://sydney.pe/wp-content/uploads/2023/09/ropa-para-verano.png'
+  },
+  {
+    id: 3,
+    name: 'Zapatos casuales',
+    price: 19.90,
+    category: 'Zapatos',
+    image: 'https://sydney.pe/wp-content/uploads/2023/09/ropa-para-verano.png'
+  },
+  {
+    id: 4,
+    name: 'Set de Brochas',
+    price: 4.75,
+    category: 'Hogar',
+    image: 'https://sydney.pe/wp-content/uploads/2023/09/ropa-para-verano.png'
+  },
+  {
+    id: 5,
+    name: 'Auriculares Bluetooth',
+    price: 29.99,
+    category: 'Electrónica',
+    image: 'https://sydney.pe/wp-content/uploads/2023/09/ropa-para-verano.png'
+  },
+  {
+    id: 6,
+    name: 'Camisa Casual',
+    price: 24.99,
+    category: 'Ropa',
+    image: 'https://sydney.pe/wp-content/uploads/2023/09/ropa-para-verano.png'
+  }
+];
